53104ccf13862bfbfa7d6178830f45328241b914
Tristen <79072811+protonvert@users.noreply.github.com>
2024-05-06 16:01:37 -0500

    implement timezone offset

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,6 +1,23 @@
-const getDateTimeFromUnixTimeStamp = (unixTimestamp) => {
-  const utcString = new Date(unixTimestamp * 1000).toUTCString();
-  return utcString;
+const MAX_RESPONSE_LENGTH = 2000;
+
+/**
+ *
+ * @param {number} unixTimestamp
+ * @param {Intl.DateTimeFormatOptions} options
+ * @returns
+ */
+const getDateTimeFromUnixTimeStamp = (
+  unixTimestamp,
+  timezoneOffset,
+  options
+) => {
+  const date = new Date(unixTimestamp * 1000);
+  if (!timezoneOffset) {
+    console.log(new Intl.DateTimeFormat('en-US', options).format(date));
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+  }
+  const localDate = new Date(date.getTime() + timezoneOffset * 1000);
+  return new Intl.DateTimeFormat('en-US', options).format(localDate);
 };
 
 const getTimeFromUnixTimestamp = (unixTimestamp) => {
@@ -18,15 +35,139 @@ const getDayFromUnixTimestamp = (unixTimestamp) => {
  * @param {string} originalString
  * @returns
  */
-const trimStringToMaxLength = (originalString) => {
-  const trimmedString = originalString.substring(0, 2000 - 3);
+const trimStringLength = (originalString, length = MAX_RESPONSE_LENGTH) => {
+  const trimmedString = originalString.substring(0, length - 3);
   const newString = trimmedString + '...';
   return newString;
 };
 
+function parseDailyWeather(data) {
+  if (!data) return '**no data available**';
+  const dailyInfo = data.daily.map((section) => {
+    const extractedInfo = {
+      dt: getDayFromUnixTimestamp(section.dt),
+      high: section?.temp?.max,
+      low: section?.temp?.min,
+      weather: section?.weather[0]?.main,
+      weatherDescription: section?.weather[0]?.description,
+    };
+
+    const displayText = `**${extractedInfo.dt}**\nHigh: ${
+      extractedInfo.high
+    }\nLow: ${extractedInfo.low}\nWeather: ${
+      extractedInfo.weather ?? 'No Data'
+    } - ${extractedInfo.weatherDescription ?? ''}\n`;
+
+    return displayText;
+  });
+
+  return dailyInfo.join('\n');
+}
+
+function parseHourlyWeather(data, timezoneOffset) {
+  if (!data) return '**no data available**';
+  const hourlyInfo = data.hourly.map((section) => {
+    const extractedInfo = {
+      dt: getDateTimeFromUnixTimeStamp(section.dt, timezoneOffset, {
+        weekday: 'short',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+        timeZone: 'UTC',
+      }),
+      temp: section.temp,
+      feelsLike: section.feels_like,
+      weather: `${section.weather[0].main} - ${section.weather[0].description}`,
+    };
+    const displayText = `**${extractedInfo.dt}**\nTemp: ${extractedInfo.temp}\nFeels Like: ${extractedInfo.feelsLike}\n${extractedInfo.weather}\n`;
+
+    return displayText;
+  });
+  return hourlyInfo.join('\n').length > MAX_RESPONSE_LENGTH
+    ? trimStringLength(hourlyInfo.join('\n'))
+    : hourlyInfo.join('\n');
+}
+
+function parseMinutelyWeather(data, timezoneOffset) {
+  if (!data) return '**no data available**';
+  const minuteInfo = data.minutely.map((section) => {
+    const extractedInfo = {
+      dt: getDateTimeFromUnixTimeStamp(section.dt, timezoneOffset, {
+        hour12: true,
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'UTC',
+      }),
+      precipitation: `${Math.round(section.precipitation * 100)}%`,
+    };
+    const displayText = `**${extractedInfo.dt}**\nPrecipitation: ${extractedInfo.precipitation}\n`;
+    return displayText;
+  });
+  return trimStringLength(minuteInfo.join('\n'), 1000);
+}
+
+function parseCurrentWeather(data, timezoneOffset) {
+  if (!data) return '**no data available**';
+  const currentInfo = {
+    dt: getDateTimeFromUnixTimeStamp(data.current.dt, timezoneOffset, {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+      timeZone: 'UTC',
+    }),
+    weather: `${data.current.weather[0]?.main} - ${data.current.weather[0]?.description}`,
+  };
+  const alertData = parseAlertData(data.alerts);
+  const displayText = `**${currentInfo.dt}**\n${currentInfo.weather}\n\n${
+    alertData ? alertData : ''
+  }`;
+  return displayText.length > MAX_RESPONSE_LENGTH
+    ? trimStringLength(displayText)
+    : displayText;
+}
+
+function parseAlertData(alerts, timezoneOffset) {
+  let alertInfo = alerts?.map((alert) => {
+    const alertObjectInfo = {
+      startDt: getDateTimeFromUnixTimeStamp(alert.start, timezoneOffset, {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'UTC',
+      }),
+      endDt: getDateTimeFromUnixTimeStamp(alert.end, timezoneOffset, {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'UTC',
+      }),
+      event: alert.event,
+      description: alert.description,
+    };
+    const displayText = `**${alertObjectInfo.event}**\n${alertObjectInfo.startDt} - ${alertObjectInfo.endDt}\n${alertObjectInfo.description}\n`;
+    return displayText;
+  });
+  if (!alertInfo) {
+    return undefined;
+  }
+  alertInfo = alertInfo.join('\n');
+  return alertInfo?.length > MAX_RESPONSE_LENGTH
+    ? trimStringLength(alertInfo)
+    : alertInfo;
+}
+
 module.exports = {
   getTimeFromUTC: getTimeFromUnixTimestamp,
-  getDateTimeFromUTC: getDateTimeFromUnixTimeStamp,
+  getDateTimeFromUnixTimeStamp,
   getDayFromUnixTimestamp,
-  trimStringToMaxLength,
+  trimStringToMaxLength: trimStringLength,
+  parseCurrentWeather,
+  parseDailyWeather,
+  parseHourlyWeather,
+  parseMinutelyWeather,
+  parseAlertData,
 };
--- helpers/weather-service.js
+++ helpers/weather-service.js
@@ -1,16 +1,42 @@
 const {
-  getTimeFromUTC,
-  getDateTimeFromUTC,
-  trimStringToMaxLength,
-  getDayFromUnixTimestamp,
+  parseCurrentWeather,
+  parseDailyWeather,
+  parseHourlyWeather,
+  parseMinutelyWeather,
+  parseAlertData,
 } = require('./utils');
 
-const MAX_RESPONSE_LENGTH = 2000;
-const mockApiCall = process.env.MOCK_API_CALL;
+const mockApiCall = JSON.parse(process.env.MOCK_API_CALL);
+
+const parseWeatherData = (forecastType, weatherInfo) => {
+  const timezoneOffset = weatherInfo?.timezone_offset;
+  let parsedData;
+  switch (forecastType) {
+    case 'daily':
+      parsedData = parseDailyWeather(weatherInfo);
+      break;
+    case 'hourly':
+      parsedData = parseHourlyWeather(weatherInfo, timezoneOffset);
+      break;
+    case 'minutely':
+      parsedData = parseMinutelyWeather(weatherInfo, timezoneOffset);
+      break;
+    case 'current':
+      parsedData = parseCurrentWeather(weatherInfo, timezoneOffset);
+      break;
+    case 'alerts':
+      parsedData = parseAlertData(weatherInfo?.alerts, timezoneOffset);
+      if (!parsedData) {
+        parsedData = 'There are no alerts for your area at this moment.';
+      }
+      break;
+  }
+  return parsedData;
+};
 
 class WeatherService {
   base_url = process.env.OPENWEATHER_URL;
-  availableForecastTypes = ['daily', 'hourly', 'minutely', 'current', 'alerts'];
+  availableForecastTypes = ['daily', 'hourly', 'minutely', 'current'];
 
   /**
    * @param {string} zipcode
@@ -50,7 +76,6 @@ class WeatherService {
     const exclusions = this.availableForecastTypes.filter(
       (forecast) => forecast !== forecastType
     );
-    let data;
 
     if (!mockApiCall) {
       const weatherResponse = await fetch(
@@ -59,123 +84,15 @@ class WeatherService {
         }&units=imperial&appid=${process.env.OPENWEATHER_APIKEY}`
       );
       const weatherInfo = await weatherResponse.json();
-      const weatherInfoForCategory = weatherInfo[forecastType];
-      switch (forecastType) {
-        case 'daily':
-          data = this.parseDailyWeather(weatherInfoForCategory);
-          break;
-        case 'hourly':
-          data = this.parseHourlyWeather(weatherInfoForCategory);
-          break;
-        case 'minutely':
-          data = this.parseMinutelyWeather(weatherInfoForCategory);
-          break;
-        case 'current':
-          data = this.parseCurrentWeather(weatherInfoForCategory);
-          break;
-      }
+      const data = parseWeatherData(forecastType, weatherInfo);
+      return data;
     } else {
       const weatherInfo = require('../testdata.json');
-      const weatherInfoForCategory = weatherInfo[forecastType];
-      switch (forecastType) {
-        case 'daily':
-          data = this.parseDailyWeather(weatherInfoForCategory);
-          break;
-        case 'hourly':
-          data = this.parseHourlyWeather(weatherInfoForCategory);
-          break;
-        case 'minutely':
-          data = this.parseMinutelyWeather(weatherInfoForCategory);
-          break;
-        case 'current':
-          data = this.parseCurrentWeather(weatherInfoForCategory);
-          break;
-      }
+      const data = parseWeatherData(forecastType, weatherInfo);
+      return data;
     }
 
     return data;
-  }
-
-  //   /**
-  //    *
-  //    * @param {string} forecastType - available options: 'hourly', 'minutely', 'daily'
-  //    */
-  //   async getAlertsForLocation(lat, lon) {
-  //     const exclusions = ['daily', 'hourly', 'minutely', 'currently', 'alerts'];
-  //     const weatherResponse = await fetch(
-  //       `${this.base_url}/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${
-  //         exclusions && [...exclusions].join(',')
-  //       }&units=imperial&appid=${process.env.OPENWEATHER_APIKEY}`
-  //     );
-  //     const data = await weatherResponse.json();
-  //     return data?.alerts;
-  //   }
-
-  parseDailyWeather(data) {
-    if (!data) return '**no data available**';
-    const dailyInfo = data.map((section) => {
-      const extractedInfo = {
-        dt: getDayFromUnixTimestamp(section.dt),
-        high: section?.temp?.max,
-        low: section?.temp?.min,
-        weather: section?.weather?.main,
-        weatherDescription: section?.weather?.description,
-      };
-
-      const displayText = `**${extractedInfo.dt}**\nHigh: ${
-        extractedInfo.high
-      }\nLow: ${extractedInfo.low}\nWeather: ${
-        extractedInfo.weather ?? 'Clear'
-      } ${extractedInfo.weatherDescription ?? ''}\n`;
-
-      return displayText;
-    });
-
-    return dailyInfo.join('\n');
-  }
-
-  parseHourlyWeather(data) {
-    if (!data) return '**no data available**';
-    const hourlyInfo = data.map((section) => {
-      const extractedInfo = {
-        dt: getDateTimeFromUTC(section.dt),
-        temp: section.temp,
-        feelsLike: section.feels_like,
-        weather: `${section.weather[0].main} - ${section.weather[0].description}`,
-      };
-      const displayText = `${extractedInfo.dt} Temp: ${extractedInfo.temp} Feels Like: ${extractedInfo.feelsLike} Weather: ${extractedInfo.weather}`;
-
-      return displayText;
-    });
-    return hourlyInfo.join('\n').length > MAX_RESPONSE_LENGTH
-      ? trimStringToMaxLength(hourlyInfo.join('\n'))
-      : hourlyInfo.join('\n');
-  }
-
-  parseMinutelyWeather(data) {
-    if (!data) return '**no data available**';
-    const minuteInfo = data.map((section) => {
-      return {
-        dt: getTimeFromUTC(section.dt),
-        precipitation: `${Math.round(section.precipitation * 100)}%`,
-      };
-    });
-    return minuteInfo;
-  }
-
-  parseCurrentWeather(data) {
-    if (!data) return '**no data available**';
-    const currentInfo = {
-      dt: getTimeFromUTC(data.dt),
-      weather: data.weather.map((section) => {
-        return {
-          main: section?.main,
-          description: section?.description,
-        };
-      }),
-      rain: data?.rain ?? 'no rain',
-    };
-    return currentInfo;
   }
 }
 
